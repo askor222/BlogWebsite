cedfc01b81a233dcb665e7204aa4e66875bb2e4a
Kibria <kibriare@gmail.com>
2023-04-03 13:54:53 +0200

    commit inicial

---
+++ main.js
@@ -0,0 +1,21 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show")
+
+});
+
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {delay: 500});
+ScrollReveal().reveal('.cards-banner-one');
+ScrollReveal().reveal('.cards-banner-two', {delay: 500});
+
+
+
+
+
+
+
+
+
+
+
